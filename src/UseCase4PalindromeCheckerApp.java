17335c90a691b4fd604e5833f0097f26429f4ccd
ar1950-adi <ar1950@srmist.edu.in>
2026-02-25 09:36:12 +0530

    Initial commit - UseCase4 Palindrome Checker App

---
+++ src/UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,32 @@
+public class UseCase4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Original string
+        String original = "level";
+
+        // Convert String to character array
+        char[] characters = original.toCharArray();
+
+        // Two-pointer approach
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(original + " is a Palindrome");
+        } else {
+            System.out.println(original + " is NOT a Palindrome");
+        }
+    }
+}
